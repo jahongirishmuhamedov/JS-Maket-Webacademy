dc5765d1af0fa9d9da15838fb274fc796fff5264
Jakhongir Ishmukhamedov <ishmuhamedovjahon@gmail.com>
2022-09-05 18:39:17 +0500

    feat: widget arrows and show more button add open and close function

--- js/main.js
+++ js/main.js
@@ -2,11 +2,28 @@ const sidebarToggleButton = document.querySelector('.menu-icon-wrapper')
 const menuIcon = document.querySelector('.menu-icon');
 const sidebar = document.querySelector('.sidebar');
 
-
 document.querySelector('.menu-icon-wrapper').onclick = function () {
     menuIcon.classList.toggle('menu-icon-active');
     sidebar.classList.toggle('sidebar--mobile-active');
-
-
 }
 
+const btnShowMoreCards = document.querySelector('.btn-more');
+const hiddenCards = document.querySelectorAll('.card-link--hidden')
+
+btnShowMoreCards.addEventListener("click", function () {
+    hiddenCards.forEach(function (card) {
+        card.classList.remove('card-link--hidden');
+    })
+})
+
+const widgets = document.querySelectorAll('.widget');
+
+widgets.forEach(function (widget) {
+    widget.addEventListener("click", function (e) {
+        if (e.target.classList.contains('widget__title')) {
+           e.target.classList.toggle('widget__title--active');
+
+           e.target.nextElementSibling.classList.toggle('widget__body--hidden');
+        }
+    })
+})
