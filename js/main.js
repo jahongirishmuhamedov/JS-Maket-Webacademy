7314e9bfd5686e18d081482ae46a6e5931a62a33
Jakhongir Ishmukhamedov <ishmuhamedovjahon@gmail.com>
2022-09-05 20:15:38 +0500

    feat: all widgets add functions

--- js/main.js
+++ js/main.js
@@ -21,9 +21,49 @@ const widgets = document.querySelectorAll('.widget');
 widgets.forEach(function (widget) {
     widget.addEventListener("click", function (e) {
         if (e.target.classList.contains('widget__title')) {
-           e.target.classList.toggle('widget__title--active');
+            e.target.classList.toggle('widget__title--active');
 
-           e.target.nextElementSibling.classList.toggle('widget__body--hidden');
+            e.target.nextElementSibling.classList.toggle('widget__body--hidden');
         }
     })
 })
+
+const checkboxAny = document.querySelector('#location-05');
+const topLocationCheckboxes = document.querySelectorAll('[data-location-parametr]')
+checkboxAny.addEventListener("change", function () {
+    if (checkboxAny.checked) {
+        topLocationCheckboxes.forEach(function (item) {
+            item.checked = false;
+        })
+        checkboxAny.checked = true;
+    }
+})
+
+topLocationCheckboxes.forEach(function (item) {
+    item.addEventListener("change", function () {
+        if (checkboxAny.checked) {
+            checkboxAny.checked = false;
+        }
+    })
+})
+
+const showMoreOptions = document.querySelector('.widget__show--hidden');
+const hiddenCheckboxes = document.querySelectorAll('.checkbox--hidden');
+
+showMoreOptions.onclick = function (e) {
+    e.preventDefault()
+    if (showMoreOptions.dataset.options === 'hidden') {
+        hiddenCheckboxes.forEach(function (item) {
+            item.style.display = 'block';
+        })
+        showMoreOptions.innerText("Скрыть опции");
+        showMoreOptions.dataset.options = 'visible'
+    }
+    else if (showMoreOptions.dataset.options === 'visible') {
+        hiddenCheckboxes.forEach(function (item) {
+            item.style.display = 'none';
+        })
+        showMoreOptions.innerText("Показать ещё");
+        showMoreOptions.dataset.options = 'hidden'
+    }
+}
